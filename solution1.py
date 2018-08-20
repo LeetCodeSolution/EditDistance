562d43a45bf6140ce73ec5a3663b7b60c4f5ae76
Germey <cqc@cuiqingcai.com>
2018-08-20 15:45:56 +0800

    update anno

--- solution1.py
+++ solution1.py
@@ -1,16 +1,25 @@
 def solution(str1, str2, i, j):
+    """
+    递归求解
+    :param str1:
+    :param str2:
+    :param i:
+    :param j:
+    :return:
+    """
     if i == 0:
         return j
     if j == 0:
         return i
-    
+    # 如果末尾相同
     if str1[i - 1] == str2[j - 1]:
         return solution(str1, str2, i - 1, j - 1)
+    # 如果末尾不同
     else:
         return min(solution(str1, str2, i - 1, j) + 1,
                    solution(str1, str2, i, j - 1) + 1,
                    solution(str1, str2, i - 1, j - 1) + 1)
 
-
-result = solution('abc', 'acbd', 3, 4)
-print(result)
+if __name__ == '__main__':
+    result = solution('abc', 'acbd', 3, 4)
+    print(result)
--- solution2.py
+++ solution2.py
@@ -1,21 +1,30 @@
 def solution(str1, str2):
+    """
+    动态规划求解
+    :param str1:
+    :param str2:
+    :return:
+    """
     str1_length = len(str1)
     str2_length = len(str2)
     
+    # 初始化矩阵
     matrix = [[0 for _ in range(str2_length)] for _ in range(str1_length)]
     
-    print(matrix)
-    
+    # 初始化边界
     for i in range(str1_length):
         matrix[i][0] = i
     for j in range(str2_length):
         matrix[0][j] = j
     print(matrix)
     
+    # 动态规划求解
     for i in range(1, str1_length):
         for j in range(1, str2_length):
+            # 如果结尾相同
             if str1[i] == str2[j]:
                 matrix[i][j] = matrix[i - 1][j - 1]
+            # 如果结尾不同
             else:
                 matrix[i][j] = min(matrix[i - 1][j] + 1,
                                    matrix[i][j - 1] + 1,
@@ -24,5 +33,6 @@ def solution(str1, str2):
     return matrix[-1][-1]
 
 
-result = solution('bcd', 'abcde')
-print(result)
+if __name__ == '__main__':
+    result = solution('bcd', 'abcde')
+    print(result)
